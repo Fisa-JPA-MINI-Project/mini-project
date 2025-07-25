d46d09de400dca69c596a887890477133dea5a5d
unknown <ricky0130@naver.com>
2025-07-25 10:18:11 +0900

    merge

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,19 +1,28 @@
 package controller;
 
+import java.io.IOException;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * Servlet implementation class UserController
- */
+
+
 @WebServlet("/userinfo")
 public class UserController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String name = req.getParameter("username");
+
+		System.out.println("입력받은 이름: " + name);
+		resp.setContentType("text/html; charset=UTF-8");
+		resp.getWriter().println("<h2>안녕하세요, " + name + "님!</h2>");
 	}
 }
